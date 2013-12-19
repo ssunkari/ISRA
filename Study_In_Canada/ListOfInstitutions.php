2c3c374bba8892d99965a4870992137228d0189f
Srinivas Sunkari <sr.sunkari@gmail.com>
2013-12-19 14:29:37 +0000

    Added testimonials and carousels

---
+++ Study_In_Canada/ListOfInstitutions.php
@@ -0,0 +1,5 @@
+<?php
+$view = new stdClass();
+$view->bannerTitle = 'common';
+$view->headerTitle = 'common';
+include_once 'scripts/ListOfInstitutions.phtml';
---
+++ js/utility.js
@@ -0,0 +1,18 @@
+﻿function placeHolder(element, txt) {
+    $(element).val(txt).css({ "color": "#adadad" });
+    
+    $(element).blur(function () {
+        var t = $(this).val();
+        if ($.trim(t) === "") {
+            $(this).val(txt).css({ "color": "#adadad" });
+        }
+    });
+
+    $(element).focus(function () {
+        var t = $(this).val();
+        $(this).css({ "color": "#000" });
+        if ($.trim(t) === txt) {
+            $(this).val('');
+        }
+    });
+}
